2af8d8ef139d0e16fc6587b759bf1d144e5e2d45
robykartis <robykartis@gmail.com>
2022-06-20 02:22:04 +0700

    commit

---
+++ src/layouts/MainLayouts.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <include-navbar />
+    <!-- End Navbar -->
+
+    <!-- Sidebar -->
+    <include-sidebar />
+    <!-- End Sidebar -->
+
+    <!-- View Page -->
+    <router-view />
+    <!-- end View Page -->
+
+    <include-footer />
+
+    <aside class="control-sidebar control-sidebar-dark"></aside>
+  </div>
+</template>
+
+<script>
+import IncludeNavbar from "./include/IncludeNavbar.vue";
+import IncludeSidebar from "./include/IncludeSidebar.vue";
+import IncludeFooter from "./include/IncludeFooter.vue";
+export default {
+  name: "MainLayouts",
+  components: {
+    IncludeNavbar,
+    IncludeSidebar,
+    IncludeFooter,
+  },
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    app: "Vue AdminLTE",
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
